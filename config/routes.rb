c3621bb07b29a44e236d5cc693c1320defde751f
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-08 15:39:23 +0200

    FB integration issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Modares::Application.routes.draw do
   devise_for :users
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   resources :levels
   
   resources :semesters
