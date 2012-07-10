bc06f177c256dce4d8afaae69ce0fb699b5a0843
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-10 17:58:28 +0200

    adding ajax requests for videos filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,20 @@ class HomeController < ApplicationController
     @subjects = Subject.all
     @videos = Video.all
   end
+  
+  def get_videos
+    @videos = Video.find_all_by_subject_id(params[:subject])
+    
+    # @videos = Video.all
+    # render :js => "alert('Blah')"
+    # respond_to do |format|
+       # format.js do
+          # render :update do |page|
+            # puts "##################################"
+            # page.replace_html 'videos-list', :partial => 'videos/video_thumb', :collection => @videos
+          # end
+       # end
+     # end
+   
+  end
 end
