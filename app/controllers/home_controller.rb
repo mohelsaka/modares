88b60bd1a5e2864a6d32bf1e07a747cd540f7a81
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-10 13:21:03 +0200

    adding video relatinos and making video thumb partial view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @levels = Level.all
     @subjects = Subject.all
+    @videos = Video.all
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,8 @@
 class Video < ActiveRecord::Base
-  attr_accessible :icon
+  attr_accessible :icon, :title, :url, :user_id, :subject_id
   has_attached_file :icon ,:styles =>{:medium => "300x300>", :thumb => "100x100>" }
+  
+  belongs_to :subject
+  belongs_to :user
+  
 end
