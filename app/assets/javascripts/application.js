f090c0e765545bc7fc9cbe6d35499da11a8a0df6
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-11 13:06:38 +0200

    Styling video result in home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,33 +9,53 @@
 //= require twitter/bootstrap
 //= require_tree .
 
-
 //= require twitter/bootstrap
 //= require bootstrap-datepicker
 
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.fr
-$(document).ready(function(){
-  $("#levels-drop-down li a").click(function(){
-  	$("#drop-down-action").html($(this).html());
-  	var id = $(this).attr('semester_id');
-  	$('#subjects li').addClass('hidden');
-  	$('#subjects li[semester='+id+']').removeClass('hidden');
-  	$("#levels-drop-down").removeClass('open');
-  	
-  	if($('#subjects-drop-down li:not(.hidden)').size()==0){
-  		$('#subjects-drop-down #subjectName').html("Empty");
-  	}else{
-  		$('#subjects-drop-down #subjectName').html("Subjects");
-  		$('#subjects-drop-down').addClass('open');  		
-  	}
-    return false;
- });
- $('.dropdown-toggle').dropdown();
- 
-  // Fix input element click problem
-  $('.dropdown input, .dropdown label').click(function(e) {
-    e.stopPropagation();
-  });
+$(document).ready(function() {
+	$("#levels-drop-down li a").click(function() {
+		$("#drop-down-action").html($(this).html());
+		var id = $(this).attr('semester_id');
+		$('#subjects li').addClass('hidden');
+		$('#subjects li[semester=' + id + ']').removeClass('hidden');
+		$("#levels-drop-down").removeClass('open');
+
+		if($('#subjects-drop-down li:not(.hidden)').size() == 0) {
+			$('#subjects-drop-down #subjectName').html("Empty");
+		} else {
+			$('#subjects-drop-down #subjectName').html("Subjects");
+			$('#subjects-drop-down').addClass('open');
+		}
+		return false;
+	});
+	$('.dropdown-toggle').dropdown();
+
+	// Fix input element click problem
+	$('.dropdown input, .dropdown label').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$('.wrapper').hover(function() {
+		$(this).find('img').animate({
+			opacity : ".6"
+		}, 300);
+		$(this).find('.caption').removeClass('hidden');
+		$(this).find('.caption').animate({
+			top : "-145px"
+		}, 300);
+	}, function() {
+		$(this).find('img').animate({
+			opacity : "1.0"
+		}, 300);
+		$(this).find('.caption').animate({
+			top : "0px"
+		}, 100);
+		$(this).find('.caption').addClass('hidden');
+	});
+	$("#videos-list").ajaxStart(function() {
+		$(this).html("<img class='offset5' id='ajax-start' src='http://www.w3schools.com/jquery/demo_wait.gif' />");
+	});
 });
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,9 @@
 class Video < ActiveRecord::Base
-  attr_accessible :icon, :title, :url, :user_id, :subject_id
+  attr_accessible :icon, :title, :url, :user_id, :subject_id, :description
   has_attached_file :icon ,:styles =>{:medium => "300x300>", :thumb => "100x100>" }
   
   belongs_to :subject
   belongs_to :user
+  
   
 end
