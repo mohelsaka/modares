170569b89c889e6c35690a0564832e90d620207f
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-12 15:29:50 +0200

    Adding positions to levels and semesters

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,4 @@
 class Level < ActiveRecord::Base
+  default_scope :order =>"position asc"
   has_many :semesters
-  
 end
--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,6 @@
 class Semester < ActiveRecord::Base
+  default_scope :order =>"position asc"
+  
   belongs_to :level
   has_many :subjects
 end
