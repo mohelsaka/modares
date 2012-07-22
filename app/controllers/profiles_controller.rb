38a6f09f663899886a2212a1158bb5d6a077cbd3
mahmoud-abdelaziz <hodafokher@gmail.com>
2012-07-22 16:18:45 +0200

    removing redundant line in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,5 @@
 class ProfilesController < ApplicationController
   def profile
     @user = User.find(params[:id])
-    
-    # Get the user's reputations
-    @reputations = RSEvaluation.where('target_owner_id'=> @user.id)    
   end
 end
