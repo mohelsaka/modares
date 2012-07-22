b47a4a8b01b1094cd0d1c8eacb55ee2b44a2ba5c
Mahmoud <mahmoud.khaled@badrit.com>
2012-07-23 00:13:39 +0200

    rm database.yml from git

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716114000) do
+ActiveRecord::Schema.define(:version => 20120718160538) do
 
   create_table "admins", :force => true do |t|
     t.string   "email"
@@ -43,6 +43,50 @@ ActiveRecord::Schema.define(:version => 20120716114000) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "rs_evaluations", :force => true do |t|
+    t.string   "reputation_name"
+    t.integer  "source_id"
+    t.string   "source_type"
+    t.integer  "target_id"
+    t.string   "target_type"
+    t.float    "value",           :default => 0.0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rs_evaluations", ["reputation_name", "source_id", "source_type", "target_id", "target_type"], :name => "index_rs_evaluations_on_reputation_name_and_source_and_target"
+  add_index "rs_evaluations", ["reputation_name"], :name => "index_rs_evaluations_on_reputation_name"
+  add_index "rs_evaluations", ["source_id", "source_type"], :name => "index_rs_evaluations_on_source_id_and_source_type"
+  add_index "rs_evaluations", ["target_id", "target_type"], :name => "index_rs_evaluations_on_target_id_and_target_type"
+
+  create_table "rs_reputation_messages", :force => true do |t|
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "receiver_id"
+    t.float    "weight",      :default => 1.0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rs_reputation_messages", ["receiver_id", "sender_id", "sender_type"], :name => "index_rs_reputation_messages_on_receiver_id_and_sender"
+  add_index "rs_reputation_messages", ["receiver_id"], :name => "index_rs_reputation_messages_on_receiver_id"
+  add_index "rs_reputation_messages", ["sender_id", "sender_type"], :name => "index_rs_reputation_messages_on_sender_id_and_sender_type"
+
+  create_table "rs_reputations", :force => true do |t|
+    t.string   "reputation_name"
+    t.float    "value",           :default => 0.0
+    t.string   "aggregated_by"
+    t.integer  "target_id"
+    t.string   "target_type"
+    t.boolean  "active",          :default => true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], :name => "index_rs_reputations_on_reputation_name_and_target"
+  add_index "rs_reputations", ["reputation_name"], :name => "index_rs_reputations_on_reputation_name"
+  add_index "rs_reputations", ["target_id", "target_type"], :name => "index_rs_reputations_on_target_id_and_target_type"
 
   create_table "semesters", :force => true do |t|
     t.string   "name"
@@ -104,5 +148,18 @@ ActiveRecord::Schema.define(:version => 20120716114000) do
     t.string   "description"
     t.integer  "duration",          :default => 0
   end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type"], :name => "index_votes_on_votable_id_and_votable_type"
+  add_index "votes", ["voter_id", "voter_type"], :name => "index_votes_on_voter_id_and_voter_type"
 
 end
