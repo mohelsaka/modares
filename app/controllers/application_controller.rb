c91b05853811ff317009e2c49a43f53372b6e070
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2012-07-22 11:54:01 +0200

    fixing redirect from authontication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,9 @@ protected
   def check_for_sign_in
     render :js => "$('#login-popup').addClass('open'); $('#signin-required').removeClass('hide');" unless user_signed_in?
   end
-  
+
+  # overwriting after_sign_in_path_for of devise gem to return the user to the same location after sign in
+  def after_sign_in_path_for(resource)
+    session[:return_to] || ''
+  end
 end
